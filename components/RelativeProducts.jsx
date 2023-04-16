899261fb9f9c89e3d0a94a273e9473be4a066c58
Abdus Samad <abdullahalsayad03@gmail.com>
2023-04-16 16:05:43 +0600

    as your like it

--- components/RelativeProducts.jsx
+++ components/RelativeProducts.jsx
@@ -27,7 +27,12 @@ const RelativeProducts = () => {
   return (
     <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
       <div className="text-2xl font-bold mb-5">You might Also like</div>
-      <Carousel responsive={responsive}>
+      <Carousel
+        responsive={responsive}
+        containerClass="-mx-[10px]"
+        itemClass="px-[10px]"
+        autoPlay={true}
+      >
         <ProductCard />
         <ProductCard />
         <ProductCard />
